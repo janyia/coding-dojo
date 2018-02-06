9005319826f634a70f8d017b6d8a2fdda49da40c
Rich <rjanyia@yahoo.com>
2018-02-06 08:07:53 -0600

    added multiples.py

---
+++ Python/multiples/multiples.py
@@ -0,0 +1,11 @@
+count =1
+while count <1001:#prints odds until 1000
+    print count
+    count +=2
+count =5
+while count< 1005: #prints 5 to a thousand, multiples of 5
+    print count
+    count +=5
+a = [1, 2, 5, 10, 255, 3]
+print sum(a)
+print (sum(a)/len(a))
