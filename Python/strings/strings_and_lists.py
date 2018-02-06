16734c8651d67f1db02e83dd2bde52fc55e9cf59
Rich <rjanyia@yahoo.com>
2018-02-06 08:03:11 -0600

    added stringsandlists.py

---
+++ Python/strings/strings_and_lists.py
@@ -0,0 +1,18 @@
+words = "It's thanksgiving day. It's my birthday,too!"
+str1 = "day"
+x = [2,54,-2,7,12,98]
+y = ["hello",2,54,-2,7,12,98,"world"]
+z = len(y)
+a = [19,2,54,-2,7,12,98,32,10,-3,6]
+b = a.sort()
+
+print words.find(str1)
+print words.replace("day" , "month")
+print "min value is: ",min(x)
+print "max value is: ",max(x)
+print "The first value is ", y[0]
+print "The last value is ",y[z-1]
+print "The new list is ",[y[0],y[z-1]]
+print "The sorted list is ",b
+
+
